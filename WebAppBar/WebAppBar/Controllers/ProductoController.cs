4929bf3cb975686b0716bbb7d18e0db5c7ddf8f5
Hectoralexa <87905201+Hectoralexan@users.noreply.github.com>
2022-06-13 18:33:35 -0600

    s
    aa

---
+++ WebAppBar/WebAppBar/Controllers/ProductoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace WebAppBar.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Add()
+        {
+            using (Models.ProductosEntities2 db = new Models.ProductosEntities2())
+            {
+                var oproducto = new Models.ListaProductos();
+                oproducto.nombre_producto = "jugoDelballe";
+                oproducto.precio_venta = 80;
+                db.ListaProductos.Add(oproducto);
+                db.SaveChanges();
+            return Ok("Hecho");
+            }
+        }
+    }
+}
